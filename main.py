e298d05211e6c16c24e2b28f5ade09ea302324f7
Uber <uber0284@gmail.com>
2022-03-14 18:49:05 +0300

    without unnes info

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from datetime import date
 from datetime import datetime as dt , timedelta
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
-# import lxml
 import time
 import csv
 
@@ -34,26 +33,26 @@ def shara_record_file():
         login = driver.find_element_by_id('log-username')
         login.clear()
         login.send_keys(user_name)
-        time.sleep(2)
+        # time.sleep(2)#2
 
         password = driver.find_element_by_id('log-password')
         password.send_keys(pass_word)
-        time.sleep(1)
+        # time.sleep(2)#4
         confirm = driver.find_element_by_xpath("/html/body/div[3]/div/form/button").click()
-        time.sleep(4)
+        time.sleep(3)#4
 
         dealer_menu = driver.find_element_by_css_selector("li.treeview:nth-child(6) > a:nth-child(1)")
-        time.sleep(4)
+        time.sleep(2)#4
         dealer_menu.click()
-        time.sleep(4)
+        time.sleep(2)#4
 
         hundred_clients = driver.find_element_by_css_selector("#users-table_length > label:nth-child(1) > select:nth-child(1) > option:nth-child(4)")
         hundred_clients.click()
-        time.sleep(3)
+        time.sleep(2)#3
 
 
         index_page = driver.page_source
-        time.sleep(3)
+        time.sleep(2)#3
 
         with open("index.html", "w", encoding="utf-8") as file:
             file.write(index_page)
@@ -83,14 +82,13 @@ def open_recorded_file():
         writer = csv.writer(file)
         writer.writerow(
 
-            ["Номер","Имя клиента","Остаток  дней", "Баланс","Пакет","Скоро закончится"]
+            ["Номер","Имя клиента","Остаток  дней", "Баланс","Пакет","Дата окончания","Статус"]
 
                       )
 
 
     client_count = 1
     for cc in client_cards[1:]:
-        # print(client_count,cc)
         client_name = cc.find("a", title="Просмотреть логи").text
         days_left = cc.find("span", class_="packet-count-left days-left") or cc.find("span", class_= "packet-count-left days-left three-or-less") or cc.find("span", class_="packet-count-left hours-left")
         ballance = cc.find("div", class_="info-tool pull-left").text
@@ -98,11 +96,9 @@ def open_recorded_file():
         last_time_used = cc.find("span", class_="packet-dates").text.split()[-1]
 
         last_day = int(last_time_used.split(".")[0])
-        last_month =  int(last_time_used.split(".")[1])
-        last_year =  int(last_time_used.split(".")[2])
-        last_time_used = dt(last_year,last_month,last_day)
-        # print("последний раз",last_time_used)
-
+        last_month = int(last_time_used.split(".")[1])
+        last_year = int(last_time_used.split(".")[2])
+        last_time_used = dt(last_year, last_month, last_day)
         soon_end_marker = ""
 
 
@@ -118,19 +114,13 @@ def open_recorded_file():
             else:
                 soon_end_marker = "Норма"
 
-
-
-
-
         else:
             days_left = "[INFO] пакет неактивен"
             if last_time_used < year_ago:
                 soon_end_marker = "Кандидат на удаление"
-            # soon_end_marker = ""
-
 
         user_data = [
-            client_count,client_name,days_left, ballance,packet_name,soon_end_marker
+            client_count,client_name,days_left, ballance,packet_name,last_time_used.strftime("%d.%m.%Y"),soon_end_marker
         ]
 
 
@@ -139,11 +129,6 @@ def open_recorded_file():
             writer.writerow(user_data)
 
         client_count +=1
-
-
-
-
-
 
 def main():
     shara_record_file()
